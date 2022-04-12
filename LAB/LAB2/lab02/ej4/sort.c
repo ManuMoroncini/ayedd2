d6bede85e14cc9b7bc7e8213342dd4523b038b22
Manu Moroncini <40662896+ManuMoroncini@users.noreply.github.com>
2022-04-12 13:36:44 -0300

    Update sort.c

--- LAB/LAB2/lab02/ej4/sort.c
+++ LAB/LAB2/lab02/ej4/sort.c
@@ -24,24 +24,81 @@ void selection_sort(int a[], unsigned int length) {
     }
 }
 
-
 static void insert(int a[], unsigned int i) {
-    /* copiá acá la implementación que hiciste en el ejercicio 1 */
+    unsigned int j = i;
+    // gb = goes_before(a[j], a[j - 1])
+    while (j > 1 && goes_before(a[j], a[j - 1])) {
+            swap(a,j-1,j);
+            j = j-1;
+        }
 }
+
+//     while (j > 1) {
+//         if (goes_before(a[j], a[j - 1])) {
+//             swap(a,j-1,j);
+//             j = j-1;
+//         }else{
+//             break;
+//         }
+//     }
+// }
+
 
 void insertion_sort(int a[], unsigned int length) {
     for (unsigned int i = 1u; i < length; ++i) {
         insert(a, i);
+        assert(array_is_sorted(a, i));
     }
 }
 
-
 static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 3 */
+    /* Needs implementation */
+
+    unsigned int i = izq+1;
+    unsigned int j = der;
+
+    while (i <= j){
+        /* if izq & der are bad located */
+        if(goes_before(a[izq], a[i]) && goes_before(a[j], a[izq])) {
+          swap(a, i, j);
+          i++;
+          j--;
+        }
+        else {
+          /* if izq is good located */
+          if(goes_before(a[i], a[izq])) {
+            i++;
+          }
+          /* if der is good located */
+          if(goes_before(a[izq], a[j])) {
+            j--;
+          }
+        }
+    }
+
+    /* swap ppiv */
+    swap(a, izq, j);
+
+    return j;
+    
+    /* Permutes elements of a[izq..der] and returns pivot such that:
+     - izq <= pivot <= der
+     - elements in a[izq,pivot) all 'go_before' (according to function goes_before) a[pivot]
+     - a[pivot] 'goes_before' all the elements in a(pivot,der]
+     - i name pivot as ppiv, manu note
+    */
 }
 
 static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 2 */
+    /* copiá acá la implementación que hiciste en el ejercicio 3 */
+    
+    unsigned int ppiv = partition(a, izq, der); 
+    if(ppiv > izq) {
+        quick_sort_rec(a, izq, ppiv-1);
+    }
+    if(ppiv < der) {
+    quick_sort_rec(a, ppiv+1, der);
+    }
 }
 
 void quick_sort(int a[], unsigned int length) {
